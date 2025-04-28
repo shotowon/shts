19cdc41d016488d54c94bbbbe501f224740ff092
shotowon <sanzhar.maratov@proton.me>
2025-04-28 08:07:01 +0500

    update main

--- cmd/shts/shts.go
+++ cmd/shts/shts.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/shotowon/shts/internal/cli"
+import (
+	"github.com/shotowon/shts/internal/cli"
+)
 
 func main() {
+	/*
+		key := "AAEEBBCCDDFFEE11"
+		msg := "hehedsadas"
+
+		encrypted, err := crypto.Encrypt(key, msg)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("encrypted: %s\n", encrypted)
+
+		decrypted, err := crypto.Decrypt(key, encrypted)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("decrypted: %s\n", decrypted)
+
+		os.Exit(0)
+	*/
 	cli.Execute()
 }
